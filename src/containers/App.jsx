ad58022c2b9e59cfb952cd34f46f6680aad8aaeb
Dina Khaled <dina.elghndour@gmail.com>
2022-01-24 14:27:55 +0200

    add folders structure and setup the needed packages (#1)

---
+++ src/containers/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+};
+
+export default App;
