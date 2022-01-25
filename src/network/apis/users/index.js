d43f931ff1938f47857f1f34ac1dfd53217d0469
DinaKhaled <dina.elghndour@gmail.com>
2022-01-25 18:30:44 +0200

    add dummy requests for delete edit update

--- src/network/apis/users/index.js
+++ src/network/apis/users/index.js
@@ -1,5 +1,11 @@
 import { axiosInstance } from "../index";
 
 const getUsers = async () => await axiosInstance.get(`data`);
+const addUser = async ({ data }) =>
+  await axiosInstance.post(`data`, data);
+const editUser = async (data) =>
+  await axiosInstance.put(`data/${data.id}`, data);
+const deleteUser = async (id) =>
+  await axiosInstance.delete(`data/${id}`);
 
-export { getUsers };
+export { getUsers, addUser, editUser, deleteUser };
